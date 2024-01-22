65777fe1e5e24562418ee379e75abd076bb0b962
Salman Khalil <122743479+salmankhalil12@users.noreply.github.com>
2024-01-23 02:44:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,151 @@
+$(document).ready(function(){
+    //==============================  Append & Prepend =================================== 
+    
+    // $("#append-btn").click(function()  { 
+        // $('#box').append("<h2>My Name is Salman </h2>");    // ".prepend" yah method div ke akhair mai  Text & div add kr dy ga 
+        
+        // });
+        // $("#prepend-btn").click(function()  {                        
+            // $('#box').prepend("<h2>My Name is Khan </h2>");    // ".prepend" yah method div sy phlay Text & div add kr dy ga 
+            
+            // });
+            
+    //==============================  AppendTo & PrependTo =================================== 
+   
+    // $("#appendTo-btn").click(function(){
+    //     $("<h1> Phlay Is mai Content add ho ga uske bad Selector ko define krain gay</h1>").appendTo("#box");
+    // })
+    // $("#prependTo-btn").click(function(){
+    //     $("<h1> Phlay Is mai Content add ho ga uske bad Selector ko define krain gay</h1>").prependTo("#box");
+    // })
+
+    //==============================  Before & After =================================== 
+    // $("#before-btn").click(function(){
+        //     $("#box").before("<h2> Add Before Text</h2>");
+        // });
+        
+        // $("#after-btn").click(function(){
+            //     $("#box").after("<h2> Add After Text</h2>");
+            // });
+            
+    //==============================  Remove & Empty =================================== 
+    
+    // $(document).ready(function(){
+        //     $("#remove-btn").click( function(){
+            //         $("#box").remove();
+            //     })
+            // })
+            // $(document).ready(function(){
+                //     $("#empty-btn").click( function(){
+                    //         $("#box").empty("div");
+                    //     })
+                    // })
+                    
+    //==============================  Remove & Empty =================================== 
+    
+    // $("#clone-btn").click(function(){
+    //     $("#box h2").clone().appendTo("#box2");     // ksi bhi div Ya Text ko Copy krny lyn Clone use krian gy
+    //     $("#box p").clone().prependTo("#box2");
+    // })
+    
+    //==============================  ReplaceWith & ReplaceAll =================================== 
+        
+    // $("#replace-btn").click(function(){
+    //     $("#box p:first").replaceWith("<h2>Salman</h2>");
+        
+    // })
+    // $("#replaceWith-btn").click(function(){
+    //     $("<h3>Hamza</h3>").replaceAll("#box");
+    // })
+    
+    //==============================  Wrap & UnWrap =================================== 
+    
+    // $("#wrap-btn").click(function(){
+        //     $("#box p:first").wrap("<div></div>")
+        // });
+        // $("#unwrap-btn").click(function(){
+            //     $("#box p").unwrap()
+            // })
+    
+            
+            //==============================  WrapAll & WrapInner =================================== 
+            
+            // $("#wrapAll-btn").click(function(){
+            //     $("p").wrapAll("<div id='test'></div>");
+            // });
+            // $("#wrapInner-btn").click(function(){
+            //     $("h2").wrapInner('<span id="test"></span>')
+            // })
+            
+            //============================== Width & Height =================================== 
+            //============================== Position & Offset =================================== 
+            //============================== ScrollTop & ScrollLeft =================================== 
+            //============================== hasclass =================================== 
+            //============================== Hide & SHow & Toggel =================================== 
+            
+            
+            // $("#hide-btn").click(function(){
+                //     // $("#box2").hide()           
+                //     $("#box2").hide("slow")           // Use in Animation
+                // })
+                // $("#show-btn").click(function(){
+                    //     // $("#box2").show()           
+                    //     $("#box2").show("slow")           // Use in Animation
+                    // })
+                    // $("#toggel-btn").click(function(){
+                        //     // $("#box2").toggle()
+                        //     $("#box2").toggle("slow")   // Use in Animation 
+                        // })
+                        
+            //============================== fadeIn & fadeOut & fadeToggel & fadeTo =================================== 
+            
+            // $("#fadeIn-btn").click(function(){
+            //     $("#box2").fadeIn(1000);
+            // })
+            // $("#fadeOut-btn").click(function(){
+            //     $("#box2").fadeOut(1000);
+            // })
+            // $("#fadeToggel-btn").click(function(){
+            //     $("#box2").fadeToggle(1000);
+            // })
+
+            // // $("Sekector").Fadeto("speed","opicity","callback");
+            // $("#fadeTo-btn").click(function(){
+            //     $("#box2").fadeTo(1000,0.2);
+            // })
+            
+            //============================== slideUp & SlideDown & SlideToggle =================================== 
+            //============================== Animation Method =================================== 
+            
+            // $("Sekector").animate({params},"speed","callback");
+            
+            // $("#Animate-btn").click(function(){
+                //     $("#box2").animate({
+                    //         marginLeft : '150px',
+                    //         fontSize: '28px',
+                    //         width: '600px',
+                    //         border: '5px'
+                    
+                    //     },2000)
+                    // })
+                    
+                    //============================== Stop Method =================================== 
+                    // stop()
+                    
+            //============================== Ancestors Method "parent, parentsUntil, closest, offsetParent" =================================== 
+
+            // div   Parent 
+            // h1    grandparent
+            // span  great-grandparent
+
+
+
+
+
+
+
+
+
+                });
+                
+                
